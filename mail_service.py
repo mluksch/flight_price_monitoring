a826c6503223d52c39ced4abf803a4169a6a119b
mluk <mikauschkal@gmail.com>
2022-01-16 21:52:55 +0100

    fix

--- mail_service.py
+++ mail_service.py
@@ -16,12 +16,15 @@ def send_simple_message(subject: str, text: str):
 
 def send_flight_price_message(home_iata: str, target_iata: str, flight: flight_service.Flight):
     send_simple_message(
-        f"Cheaper Flight: {home_iata} to {target_iata}for Eur{'{:.2f}'.format(flight.price)}", f"""
+        f"Cheaper Flight: {home_iata} to {target_iata} for Eur{'{:.2f}'.format(flight.price)}", f"""
     Cheaper Flight detected: {home_iata} to {target_iata}
+    
     Price: Eur{'{:.2f}'.format(flight.price)}
     Departure: {flight.utc_departure}
     Arrival: {flight.utc_arrival}
     Duration: {flight.duration}h
-    Link: {flight.link}
-    Sheet: {config.SHEET_LINK}
+    
+    -> Book this flight: {flight.link}
+    
+    -> Go to monitored flights: {config.SHEET_LINK}
     """)
